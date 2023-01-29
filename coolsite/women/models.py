dc779ed38879bc8012a48a562c4bd63119737408
Артём Климов <temni@mail.ru>
2023-01-29 20:14:13 +0700

    Anothrer commit

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.urls import reverse
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
+
 class Women(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
@@ -9,9 +13,11 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True, blank=True)
     is_published = models.BooleanField(default=True, blank=True)
+    cat = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True,
+                            related_name='women')
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('women:post', kwargs ={'post_id': self.pk})
+        return reverse('women:post', kwargs={'post_id': self.pk})
